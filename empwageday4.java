6af8d1b5f232ad93c451d1b8455b40045bfc2202
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-01 11:21:25 +0530

    adding wage permonth

--- empwageday4.java
+++ empwageday4.java
@@ -5,26 +5,27 @@ public class empwageday4 {
 	public static final int IS_PART_PRESENT=2;
 	public static void main(String[] args){
 		System.out.println("welcome to Employee Wage Computation");
-		int wage_per_hr=20;
-		int full_day_work_hr=8;
-		int part_day_work_hr=4; 
+		int wage_per_hr=20,full_day_work_hr=8,part_day_work_hr=4; 
 		int salary;
-		double random=Math.floor(Math.random()*10)%3;
-		switch ((int)random) {
-		case IS_FULL_PRESENT : 
-			System.out.println("employee is present");
-			salary=(wage_per_hr*full_day_work_hr);
-			System.out.println("salary is:"+salary);
-			break;
-		case IS_PART_PRESENT:
-			salary=(wage_per_hr*part_day_work_hr);
-			System.out.println("salary is :"+salary);
-			System.out.println("employee is part time present");
-		    break;
-		default:
-			System.out.println("when employee is absent for the day his salary is zero");					
+		int working_days=20;
+		int salary_per_month=0;
+		for (int i=1;i<working_days;i++)
+		{
+			double random= Math.floor(Math.random()*10)%2;
+			if (IS_FULL_PRESENT==random)
+			{
+				salary=(wage_per_hr*full_day_work_hr);
+				salary_per_month=(salary_per_month+salary);
+			}
+			else if (IS_PART_PRESENT==random)
+			{
+				salary=(wage_per_hr*part_day_work_hr);
+				salary_per_month=(salary_per_month+salary);
+			}
 		}
-	}
+		System.out.println("salary per month is"+salary_per_month);
+	}					
 }
+
 
 
