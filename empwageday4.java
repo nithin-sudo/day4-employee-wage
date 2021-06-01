e04f795222a48b1084e92954d2f8c8cd15c00f15
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-01 11:39:12 +0530

    calculating employee wage until it meets a certain condition

--- empwageday4.java
+++ empwageday4.java
@@ -5,26 +5,33 @@ public class empwageday4 {
 	public static final int IS_PART_PRESENT=2;
 	public static void main(String[] args){
 		System.out.println("welcome to Employee Wage Computation");
-		int wage_per_hr=20,full_day_work_hr=8,part_day_work_hr=4; 
-		int salary;
-		int working_days=20;
+		int wage_per_hr=20;
+		int full_day_work_hr=8;
+		int part_day_work_hr=4; 
+		int salary,working_days=20;
 		int salary_per_month=0;
-		for (int i=1;i<working_days;i++)
-		{
-			double random= Math.floor(Math.random()*10)%2;
-			if (IS_FULL_PRESENT==random)
-			{
+		int total_working_hrs=0;
+		int i;
+	  for (i=0;i<working_days;i++)
+	  {
+		  double random= Math.floor(Math.random()*10)%2;
+			switch((int)random) {
+			case IS_FULL_PRESENT:
 				salary=(wage_per_hr*full_day_work_hr);
 				salary_per_month=(salary_per_month+salary);
-			}
-			else if (IS_PART_PRESENT==random)
-			{
+				total_working_hrs=(total_working_hrs+full_day_work_hr);
+				break;
+			case IS_PART_PRESENT:
 				salary=(wage_per_hr*part_day_work_hr);
 				salary_per_month=(salary_per_month+salary);
-			}
+				total_working_hrs=(total_working_hrs+part_day_work_hr);
+				break;
+		  }
+			if (total_working_hrs==100 || i==20)
+				break;
 		}
-		System.out.println("salary per month is"+salary_per_month);
-	}					
+	System.out.println("salary per month " + salary_per_month );
+}					
 }
 
 
