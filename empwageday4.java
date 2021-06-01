cb3b9574bc1e7785d1b4baf64a284a0ebf2c096c
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-01 11:01:30 +0530

    adding employee wage

--- empwageday4.java
+++ empwageday4.java
@@ -3,17 +3,14 @@ package empwage;
 public class empwageday4 {
 	public static void main(String[] args){
 		System.out.println("welcome to Employee Wage Computation");
-		int is_full_present=1;
+		int is_full_present=1,wage_per_hr=20,full_day_work_hr=0;
+		int salary;
 		double random=Math.floor(Math.random()*10)%2;
-		System.out.println("random value is:"+random);
 		if (random==is_full_present) 
-		{
-			System.out.println("employee is present");
-		}
-		else
-		{
-			System.out.println("employee is absent");			
-		}
+			full_day_work_hr=8;
+		salary=(wage_per_hr*full_day_work_hr);
+		System.out.println(salary);		
 	}
-
 }
+
+
